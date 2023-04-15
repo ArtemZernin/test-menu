8ba740742bccb0431fdbc32fa8a29c5a323b7289
Artem Zernin <azernin.edadil@yandex.ru>
2023-04-15 21:02:49 +0000

    add submenu

--- test_menu/menu/templatetags/menu.py
+++ test_menu/menu/templatetags/menu.py
@@ -5,8 +5,16 @@ register = template.Library()
 
 @register.inclusion_tag('includes/menu.html')
 def draw_menu(slug):
-    menu_list = Menu.objects.filter(parent=None)
+    menu_list = Menu.objects.filter(slug=slug)
     context = {
-        'menu_item': menu_list,
+        'menu_items': menu_list,
     }
     return context
+
+@register.inclusion_tag('includes/submenu.html')
+def draw_submenu(m_item, m_list):
+    m_list1 = []
+    for item in m_list:
+        if m_item == item.parent:
+            m_list1.append(item)  
+    return {'m_items': m_list1, 'menu_items': m_list}
--- test_menu/menu/views.py
+++ test_menu/menu/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .models import Menu
 
 
-def index(request):    
+def index(request):
     return render(request, 'page.html')
-    
+
+def page(request, slug, title):
+    return render(request, 'page.html')
